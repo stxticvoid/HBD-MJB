c67d9539e69453e2339abeaf623487b6b4379f00
stxticvoid <benfranklinalojado@gmail.com>
2025-03-02 23:19:27 +0800

    hbd po

--- assets/jvascrpt/type.js
+++ assets/jvascrpt/type.js
@@ -1,13 +1,5 @@
 var i = 0;
-var txt = '<br><br> Hello Mika, <br>
-I just wanted to take a moment to tell you how much I appreciate you, it’s because of Arrupe Social Formation we’ve got to know each other. Nag effort ka gid na sulatan mo ko sang letter ba haha. Thank you for being patient with me in times na grabe ko ka busy, lagaw lang ta sunod ah. I thank you also for introducing me to ACAC, nag ka org ko biglaan na upod ta haha. I’m honestly so lucky to call you my friend.<br><br> 
-
-I hope today is everything you deserve and more, and that this year brings you all the things you’ve been dreaming of. You truly deserve all the happiness in the world, and I can’t wait to see all the incredible things you’ll accomplish. Continue to strive for the best. <br><br>
-
-Here’s to another year of laughter, new adventures, and making unforgettable memories. Enjoy every moment of your special day! <br><br>
-
- - Toto  MJ <br>';
-
+var txt = '<br><br> Hello Mika, <br><br> I just wanted to take a moment to tell you how much I appreciate you, it’s because of Arrupe Social Formation we’ve got to know each other. Nag effort ka gid na sulatan mo ko sang letter ba haha. Thank you for being patient with me in times na grabe ko ka busy, lagaw lang ta sunod ah. I thank you also for introducing me to ACAC, nag ka org ko biglaan na upod ta haha. I’m honestly so lucky to call you my friend.<br><br> I hope today is everything you deserve and more, and that this year brings you all the things you’ve been dreaming of. You truly deserve all the happiness in the world, and I can’t wait to see all the incredible things you’ll accomplish. Continue to strive for the best. <br><br> Here’s to another year of laughter, new adventures, and making unforgettable memories. Enjoy every moment of your special day! <br><br> - Toto MJ <br>';
 var speed = 50;
 var isTypingDone = false; // Global variable to track typing status
 
